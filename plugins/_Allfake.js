ff24a23ed78ad1d48884333d0da378c7913d0651
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-22 22:17:38 -0500

    Update _Allfake.js

--- plugins/_Allfake.js
+++ plugins/_Allfake.js
@@ -6,7 +6,15 @@ import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
-
+//******* 🅒︎🅡︎🅔︎🅓︎🅘︎🅣︎🅞︎🅢︎ *****/
+global.hachejota = '𝙷𝙰𝙲𝙷𝙴𝙹𝙾𝚃𝙰';
+global.hb = 'HΔCHIҜΩβΩT-MD';
+global.wm = '𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃-𝙼𝙳';
+global.titulowm2 = `乂  𝐇 𝐀 𝐂 𝐇 𝐈 𝐊 𝐎 - 𝐁 𝐎 𝐓 - 𝐌 𝐃 乂`;
+global.gp1 = 'https://chat.whatsapp.com/LRQdtS7nKt89NBUPWJDjzh';
+global.md = 'https://www.instagram.com/hachejota16';
+global.suittag = ['5214437863111']
+//******=****-*-***=*==================*
 global.fgclink = { key: { fromMe: false, participant: "0@s.whatsapp.net", remoteJid: "0@s.whatsapp.net" }, message: { groupInviteMessage: { groupJid: "6285736178354-1625305606@g.us", inviteCode: null, groupName:`${hb}`, caption: hb, jpegThumbnail: imagen4}}};
 
 global.fdocs = { key: { participant : "0@s.whatsapp.net" }, message: { documentMessage: { title: wm, jpegThumbnail: imagen4}}};
@@ -56,14 +64,7 @@ global.estilo = { key: {  fromMe: false, participant: "0@s.whatsapp.net", ...(fa
 global.twa = {key: {participant: "0@s.whatsapp.net", remoteJid: "0@s.whatsapp.net"}, message: { groupInviteMessage: { groupJid: "51995386439-1616969743@g.us", inviteCode: "m", groupName: "P", caption: wm, jpegThumbnail: imagen4}}};
 
 global.fakemek = {key: {participant: "0@s.whatsapp.net",remoteJid: "0@s.whatsapp.net"}, message: {groupInviteMessage: {groupJid: "51995386439-1616969743@g.us", inviteCode: "m",groupName: "P", caption: 'CuriosityBot-MD', jpegThumbnail: null}}};
-//******* 🅒︎🅡︎🅔︎🅓︎🅘︎🅣︎🅞︎🅢︎ *****/
-global.hachejota = '𝙷𝙰𝙲𝙷𝙴𝙹𝙾𝚃𝙰';
-global.hb = 'HΔCHIҜΩβΩT-MD';
-global.wm = '𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃-𝙼𝙳';
-global.gp1 = 'https://chat.whatsapp.com/LRQdtS7nKt89NBUPWJDjzh';
-global.md = 'https://www.instagram.com/hachejota16';
-global.suittag = ['5214437863111']
-//************************************************/
+
 var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'una linda noche 🌙'; break; case 1: hour = 'una linda noche 💤'; break; case 2: hour = 'una linda noche 🦉'; break; case 3: hour = 'una linda mañana ✨'; break; case 4: hour = 'una linda mañana 💫'; break; case 5: hour = 'una linda mañana 🌅'; break; case 6: hour = 'una linda mañana 🌄'; break; case 7: hour = 'una linda mañana 🌅'; break; case 8: hour = 'una linda mañana 💫'; break; case 9: hour = 'una linda mañana ✨'; break; case 10: hour = 'un lindo dia 🌞'; break; case 11: hour = 'un lindo dia 🌨'; break; case 12: hour = 'un lindo dia ❄'; break; case 13: hour = 'un lindo dia 🌤'; break; case 14: hour = 'una linda tarde 🌇'; break; case 15: hour = 'una linda tarde 🏜️'; break; case 16: hour = 'una linda tarde ☀️'; break; case 17: hour = 'una linda tarde 🌆'; break; case 18: hour = 'una linda noche 🌙'; break; case 19: hour = 'una linda noche 🌃'; break; case 20: hour = 'una linda noche 🌌'; break; case 21: hour = 'una linda noche 🌃'; break; case 22: hour = 'una linda noche 🌙'; break; case 23: hour = 'una linda noche 🌃'; break;}
 global.saludo = "Espero que tengas " + hour;
 
