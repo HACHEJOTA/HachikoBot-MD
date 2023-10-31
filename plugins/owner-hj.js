b0709deab38e512564f629254c8107fdb4b0f10c
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-10-31 18:26:38 -0500

    Update owner-hj.js

--- plugins/owner-hj.js
+++ plugins/owner-hj.js
@@ -9,7 +9,7 @@ handler.all = async function (m) {
     //sem prefixo
     /*conn.reply(
       m.chat,
-      `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${global.suittag} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`,
+      `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${global.suittag} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`,fliveLoc,
       m
     );*/ //wm, null, [['Menu', '#menu']], m) botones :V
   }
