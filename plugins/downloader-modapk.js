21f3e606f713ce1e7f0b392919fd792bfc9047be
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-02 07:58:37 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -4,14 +4,14 @@ const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = ` *დ 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝙳𝙾𝚁 𝙳𝙴 𝙰𝙿𝙺𝚂 დ* \n\n📌 *𝙽𝙾𝙼𝙱𝚁𝙴:* ${data5.name}\n📦 *𝙿𝙰𝙲𝙺𝙰𝙶𝙴:* ${data5.package}\n🕒 *𝙴𝙻𝚃𝙸𝙼𝙰 𝙰𝙲𝚃𝚄𝙰𝙻𝙸𝚉𝙰𝙲𝙸𝙾𝙽:* ${data5.lastup}\n📥 *𝚃𝙰𝙼𝙰𝙽̃𝙾:* ${data5.size}`
+    let response = ` *დ 𝙳𝙴𝚂𝙲𝙰𝚁𝙶𝙰𝙳𝙾𝚁 𝙳𝙴 𝙰𝙿𝙺𝚂 დ* \n\n📌 *𝙽𝙾𝙼𝙱𝚁𝙴:* ${data5.name}\n📦 *𝙿𝙰𝙲𝙺𝙰𝙶𝙴:* ${data5.package}\n🕒 *𝚄𝙻𝚃𝙸𝙼𝙰 𝙰𝙲𝚃𝚄𝙰𝙻𝙸𝚉𝙰𝙲𝙸𝙾𝙽:* ${data5.lastup}\n📥 *𝚃𝙰𝙼𝙰𝙽̃𝙾:* ${data5.size}`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] єℓ αя¢нινσ єѕ ∂ємαѕια∂σ ρєѕα∂σ ρσя ℓσ qυє иσ ѕє єиνιαяá.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] 𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 𝙴𝚂 𝙳𝙴𝙼𝙰𝙲𝙸𝙰𝙳𝙾 𝙿𝙴𝚂𝙰𝙳𝙾 𝙿𝙾𝚁 𝙻𝙾 𝚀𝚄𝙴 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙰.*'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `*[❗] 𝙴𝚛𝚛𝚘𝚛, 𝚗𝚘 𝚜𝚎 𝚎𝚗𝚌𝚘𝚗𝚝𝚛𝚊𝚛ó𝚗 𝚛𝚎𝚜𝚞𝚕𝚝𝚊𝚍𝚘𝚜 𝚙𝚊𝚛𝚊 𝚜𝚞 𝚋ú𝚜𝚚𝚞𝚎𝚍𝚊.*`;
+    throw `*[❗] 𝙀𝙧𝙧𝙤𝙧 𝙣𝙤 𝙨𝙚 𝙚𝙣𝙘𝙤𝙣𝙩𝙖𝙧𝙤𝙣 𝙧𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤𝙨 𝙥𝙖𝙧𝙖 𝙨𝙪 𝙗𝙪𝙨𝙦𝙪𝙚𝙙𝙖.*`;
   }    
 };
 handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
