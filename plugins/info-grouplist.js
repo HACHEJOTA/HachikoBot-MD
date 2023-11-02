0635a2e056603b75e1e7ebf53b87180fa6dc607d
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-02 16:54:42 -0500

    Actualizar info-grouplist.js

--- plugins/info-grouplist.js
+++ plugins/info-grouplist.js
@@ -2,8 +2,8 @@ const handler = async (m, {conn}) => {
   let txt = '';
   for (const [jid, chat] of Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats)) txt += `\n🜺 ${await conn.getName(jid)}\n➤ ${jid} [${chat?.metadata?.read_only ? '𝙽𝙾 𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴𝚂' : '𝙿𝙰𝚁𝚃𝙸𝙲𝙸𝙿𝙰𝙽𝚃𝙴'}]\n\n`;
   m.reply(`[🐶] 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙶𝚁𝚄𝙿𝙾𝚂 𝙴𝙽 𝙴𝙻 𝚀𝚄𝙴 𝙴𝚂𝚃𝙰 𝙷𝙰𝙲𝙷𝙸𝙺𝙾-𝙱𝙾𝚃.
-𝚃𝙾𝚃𝙰𝙻: ${groups.length}
- ${txt}
+
+🌴 ${txt}
 `.trim());
 };
 handler.help = ['groups', 'grouplist'];
