2101f4fba7903321cdd7f133c06f000c262249e8
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-02 07:15:45 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -2,7 +2,11 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
-  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') ||m.text.includes('descargarbot')|| m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') ||m.text.includes('descargarbot')||
+m.text.includes('join') ||
+m.text.includes('menu')||
+m.text.includes('owner')||
+m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
