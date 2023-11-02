6b687f22ef9a112705dffc244067e4bb6220ac8e
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-02 00:23:48 -0500

    Actualizar owner-hj.js

--- plugins/owner-hj.js
+++ plugins/owner-hj.js
@@ -5,11 +5,11 @@ handler.all = async function (m) {
   if (/^HacheJota|Hj|hj|hachejota|Hachejota|HACHEJOTA$/i.test(m.text)) {
     var hj = `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${global.suittag} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`;
 
-    m.reply(hj, m.chat,{ mentions: conn.parseMention(hj)});
-    //sem prefixo
-    /*conn.reply(
+    //m.reply(hj, m.chat,{ mentions: conn.parseMention(hj)});
+    sem prefixo
+    conn.reply(
       m.chat,
-      `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${global.suittag} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`,fliveLoc,
+      `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${hachejota} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`,fliveLoc,
       m
     );*/ //wm, null, [['Menu', '#menu']], m) botones :V
   }
