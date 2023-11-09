9c15152a7b10ffa8f772f5af68c2d2cc023bf591
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-09 17:51:47 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
-console.log("☑️ㅤIniciando bot...");
-import {join, dirname} from "path";
-import {fileURLToPath} from "url";
-import {setupMaster, fork} from "cluster";
-import cfonts from "cfonts";
-import {createInterface} from "readline";
-import yargs from "yargs";
+console.log('✅ㅤIniciando Hachiko-Bot-MD...');
+import {join, dirname} from 'path';
+import {createRequire} from 'module';
+import {fileURLToPath} from 'url';
+import {setupMaster, fork} from 'cluster';
+import {watchFile, unwatchFile} from 'fs';
+import cfonts from 'cfonts';
+import {createInterface} from 'readline';
+import yargs from 'yargs';
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const require = createRequire(__dirname);
+const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say("THE DORRAT-BOT-MD", {
-  font: "tiny",
-  align: "center",
-  colors: ["#ff8000"],
-});
-say(`CREADO POR DIEGO-OFC`, {
-  font: "console",
-  align: "center",
-  colors: ["red"],
-});
+say('Hachiko - Bot \nMD', {
+  font: 'tiny',
+  align: 'center',
+  gradient: ['red', 'magenta']});
+say(`By HacheJota`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']});
 
-var isRunning = false
+let isRunning = false;
 /**
 * Start a js file
 * @param {String} file `path/to/file`
 */
 function start(file) {
-if (isRunning) return
-isRunning = true
-let args = [join(__dirname, file), ...process.argv.slice(2)]
-  
-setupMaster({
-exec: args[0],
-args: args.slice(1), })
-let p = fork()
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-break
-case 'uptime':
-p.send(process.uptime())
-break }})
-p.on('exit', (_, code) => {
-isRunning = false
-console.error('⚠️ Error Inesperado ⚠️', code)
-  
-p.process.kill()
-isRunning = false
-start.apply(this, arguments)
-  
-if (process.env.pm_id) {
-process.exit(1)
-} else {
-process.exit()
+  if (isRunning) return;
+  isRunning = true;
+  const args = [join(__dirname, file), ...process.argv.slice(2)];
+
+  say('Elije una de las dos opciones', {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'blue']});
+
+  setupMaster({
+    exec: args[0],
+    args: args.slice(1)});
+  const p = fork();
+  p.on('message', (data) => {
+    switch (data) {
+      case 'reset':
+        p.process.kill();
+        isRunning = false;
+        start.apply(this, arguments);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
+    }
+  });
+  p.on('exit', (_, code) => {
+    isRunning = false;
+    console.error('❗ㅤOcurrio un error inesperado:', code);
+
+    p.process.kill();
+    isRunning = false;
+    start.apply(this, arguments);
+
+    if (process.env.pm_id) {
+      process.exit(1);
+    } else {
+      process.exit();
+    }
+  });
+  const opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse());
+  if (!opts['test']) {
+    if (!rl.listenerCount()) {
+      rl.on('line', (line) => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
 }
-})
-let opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
-if (!opts['test'])
-if (!rl.listenerCount()) rl.on('line', line => {
-p.emit('message', line.trim())
-})}
-start('main.js')
+start('main.js');
