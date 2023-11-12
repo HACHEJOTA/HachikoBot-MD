32aa3f84cfa532310c79f0bc92821c93b085b36b
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-11 22:28:09 -0500

    Actualizar downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,6 +1,6 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*[❗] ɪɴɢʀᴇsᴇ ᴇʟ ɴᴏᴍʙʀᴇ ᴅᴇ ʟᴀ ᴀᴘᴋ ǫᴜᴇ ǫᴜɪᴇʀᴀ ʙᴜsᴄᴀʀ.*`;
+ if (!text) throw `*❗ ɪɴɢʀᴇsᴇ ᴇʟ ɴᴏᴍʙʀᴇ ᴅᴇ ʟᴀ ᴀᴘᴋ ǫᴜᴇ ǫᴜɪᴇʀᴀ ʙᴜsᴄᴀʀ.*`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
@@ -20,11 +20,11 @@ renderLargerThumbnail: true
 
 //await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] 𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 𝙴𝚂 𝙳𝙴𝙼𝙰𝙲𝙸𝙰𝙳𝙾 𝙿𝙴𝚂𝙰𝙳𝙾 𝙿𝙾𝚁 𝙻𝙾 𝚀𝚄𝙴 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙰.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '* ⛔  𝙴𝙻 𝙰𝚁𝙲𝙷𝙸𝚅𝙾 𝙴𝚂 𝙳𝙴𝙼𝙰𝙲𝙸𝙰𝙳𝙾 𝙿𝙴𝚂𝙰𝙳𝙾 𝙿𝙾𝚁 𝙻𝙾 𝚀𝚄𝙴 𝙽𝙾 𝚂𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙰.*'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `*[❗] 𝙀𝙧𝙧𝙤𝙧 𝙣𝙤 𝙨𝙚 𝙚𝙣𝙘𝙤𝙣𝙩𝙖𝙧𝙤𝙣 𝙧𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤𝙨 𝙥𝙖𝙧𝙖 𝙨𝙪 𝙗𝙪𝙨𝙦𝙪𝙚𝙙𝙖.*`;
+    throw `*❗ 𝙀𝙧𝙧𝙤𝙧 𝙣𝙤 𝙨𝙚 𝙚𝙣𝙘𝙤𝙣𝙩𝙖𝙧𝙤𝙣 𝙧𝙚𝙨𝙪𝙡𝙩𝙖𝙙𝙤𝙨 𝙥𝙖𝙧𝙖 𝙨𝙪 𝙗𝙪𝙨𝙦𝙪𝙚𝙙𝙖.*`;
   }    
 };
 handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
