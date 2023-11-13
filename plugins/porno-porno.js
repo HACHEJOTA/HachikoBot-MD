d6195d034f245ab3ca80c902bfc23c4becf0316b
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-12 22:45:39 -0500

    Actualizar porno-porno.js

--- plugins/porno-porno.js
+++ plugins/porno-porno.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let res = await fetch('https://api.zahwazein.xyz/randomasupan/discord18?apikey=zenzkey_048b3b850d51')
 let json = await res.json()
 conn.sendMessage(m.chat, { video: { url: json.result }, mimetype: "video/mp4", fileName: 'xd' + '.mp4'},{ quoted: m })
-}
+};
 handler.tags = ["info"];
 handler.command = handler.help = ["hjpajero", "hjporno"];
 handler.premiun = true;
