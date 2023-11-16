dd2e810801af7fe0ac70fd6bdd9c647d55a2d901
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-15 21:55:00 -0500

    Actualizar aaa-prueba.js

--- plugins/aaa-prueba.js
+++ plugins/aaa-prueba.js
@@ -5,8 +5,8 @@ const exec = promisify(_exec).bind(cp);
 const handler = async (m, {conn, isROwner, usedPrefix, command, text}) => {
   const ar = Object.keys(plugins);
   const ar1 = ar.map((v , i) => 
-
-conn.reply(m.chat,`${ar1}`,m)
+const tlp = `${ar1.map(v => `├❥ᰰຼ ${v}`}`
+conn.reply(m.chat,`${tlp}`,m)
 }
 
 handler.tags = ['owner'];
@@ -15,7 +15,7 @@ handler.rowner = true;
 export default handler;
 
 /**╭─╮─᤻─᳒─᤻᳒᯽⃟ᰳᰬᰶ┈*⃐ *Lista de Plugins*️⃟ᬽ፝֟━*
-├❥ᰰຼ ${ar1.map(v => `├❥ᰰຼ ${v}`).join('\n')}
+├❥ᰰຼ ${ar1.map(v => `├❥ᰰຼ ${v}`}.join('\n')}
 *╰┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄̸࣭࣭࣭࣭࣭ٜ۫┄
 (`* 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙿𝙻𝚄𝙶𝙸𝙽𝚂 (𝙰𝚁𝙲𝙷𝙸𝚅𝙾𝚂) 𝙴𝚇𝙸𝚂𝚃𝙴𝙽𝚃𝙴𝚂:*\n*➵* ${ar1.map((v) => ' ' + v).join`\n*➻*`}`);
 ۫/*
