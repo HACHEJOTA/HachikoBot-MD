c1bacc39d386fa6eb5322f113211f6efe0d80d3f
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-15 22:13:12 -0500

    Actualizar aaa-prueba.js

--- plugins/aaa-prueba.js
+++ plugins/aaa-prueba.js
@@ -4,13 +4,27 @@ import fs from 'fs';
 const exec = promisify(_exec).bind(cp);
 const handler = async (m, {conn, isROwner, usedPrefix, command, text}) => {
   const ar = Object.keys(plugins);
-  let ar1 = ar.map((v) => 
-
-conn.reply(m.chat,`${ar1}`,m)
-}
-
+  const ar1 = ar.map((v, i) => v.replace('.js', ''));
+  if (!ar1.includes(text)) return m.reply(`* 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙿𝙻𝚄𝙶𝙸𝙽𝚂 (𝙰𝚁𝙲𝙷𝙸𝚅𝙾𝚂) 𝙴𝚇𝙸𝚂𝚃𝙴𝙽𝚃𝙴𝚂:*\n*➵* ${ar1.map((v) => ' ' + v).join`\n*${i + 1}*`}`);
+  let o;
+  try {
+    o = await exec('cat plugins/' + text + '.js');
+  } catch (e) {
+    o = e;
+  } finally {
+    const {stdout, stderr} = o;
+    if (stdout.trim()) {
+      const aa = await conn.sendMessage(m.chat, {text: stdout}, {quoted: m});
+      await conn.sendMessage(m.chat, {document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js`}, {quoted: aa});
+    }
+    if (stderr.trim()) {
+      const aa2 = await conn.sendMessage(m.chat, {text: stderr}, {quoted: m});
+      await conn.sendMessage(m.chat, {document: fs.readFileSync(`./plugins/${text}.js`), mimetype: 'application/javascript', fileName: `${text}.js`}, {quoted: aa2});
+    }
+  }
+};
+handler.help = ['getplugin'].map((v) => v + ' *<nombre>*');
 handler.tags = ['owner'];
 handler.command = /^(totalp|tlp)$/i;
 handler.rowner = true;
 export default handler;
-
