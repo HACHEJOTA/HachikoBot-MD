f44bd7807ab5782da7ca92ea175af6f48db7a598
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-15 22:54:43 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -5,16 +5,16 @@ const handler = async (m, {conn, text}) => {
   if (!text) throw '⚠️ *_Que quieres que busque en YouTube?_*';
   const results = await yts(text);
   const tes = results.all;
-  const teks = results.all.map((v) => {
+  const teks = results.all.map((v, h) => {
     switch (v.type) {
       case 'video': return `
- 🏮 *_𝚃𝙸𝚃𝚄𝙻𝙾 :_* ${v.title}
+ ${h + 1} *_𝚃𝙸𝚃𝚄𝙻𝙾 :_* ${v.title}
  🔗 *_𝙻𝙸𝙽𝙺 :_* ${v.url}
  🕒 *_𝙳𝚄𝚁𝙰𝙲𝙸𝙾𝙽 :_* ${v.timestamp}
  📥 *_𝚂𝚄𝙱𝙸𝙳𝙾 :_* ${v.ago}
  👁 *_𝚅𝙸𝚂𝚃𝙰𝚂 :_* ${v.views}`;
     }                            
-  }).filter((v) => v).join('\nꙬꙬꙬꙬꙬꙬꙬꙬꙬꙬ\n');
+  }).filter((v) => v).join('\n\nꙬꙬꙬꙬꙬꙬꙬꙬꙬꙬ\n\n');
   conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m);
 };
 handler.help = ['ytsearch *<texto>*'];
