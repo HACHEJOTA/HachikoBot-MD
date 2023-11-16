bdc6ee41bc3de424cfd25e69a86f300dd48b0dfc
HACHE-JOTA <131606359+HACHEJOTA@users.noreply.github.com>
2023-11-15 21:49:56 -0500

    Actualizar aaa-prueba.js

--- plugins/aaa-prueba.js
+++ plugins/aaa-prueba.js
@@ -4,14 +4,19 @@ import fs from 'fs';
 const exec = promisify(_exec).bind(cp);
 const handler = async (m, {conn, isROwner, usedPrefix, command, text}) => {
   const ar = Object.keys(plugins);
-  let ar1 = ar.map((v , i) => v.replace('.js', '')) => {
-  if (!ar1.includes(text)) return m.reply(`TODOS LOS PLUGINS\n ${ar1.map(v => `  ${i + 1`} ${v}`).join('\n')};
- m.reply(`${ar1}`)
+  const ar1 = ar.map((v , i) => v.replace('.js', '')) 
+let totalp = ar1.map(v => ` ${i + 1} ${v}`).join('\n')
+await conn.reply(m.chat,`${totalp}`,m)
 }
-
 
 handler.tags = ['owner'];
 handler.command = /^(totalp|tlp)$/i;
 handler.rowner = true;
 export default handler;
+
+/**╭─╮─᤻─᳒─᤻᳒᯽⃟ᰳᰬᰶ┈*⃐ *Lista de Plugins*️⃟ᬽ፝֟━*
+├❥ᰰຼ ${ar1.map(v => `├❥ᰰຼ ${v}`).join('\n')}
+*╰┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄࣭࣭࣭۫┄̸࣭۫┄̸࣭࣭࣭࣭࣭ٜ۫┄̸࣭࣭࣭࣭࣭ٜ۫┄
+(`* 𝙻𝙸𝚂𝚃𝙰 𝙳𝙴 𝙿𝙻𝚄𝙶𝙸𝙽𝚂 (𝙰𝚁𝙲𝙷𝙸𝚅𝙾𝚂) 𝙴𝚇𝙸𝚂𝚃𝙴𝙽𝚃𝙴𝚂:*\n*➵* ${ar1.map((v) => ' ' + v).join`\n*➻*`}`);
+۫/*
 
